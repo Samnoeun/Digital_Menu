95b77f7e46109baf69ce91c436ddffc860a68425
neatsolin <neatsolin98@gmail.com>
2025-08-01 20:07:18 +0700

    fix conflict

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/Login/login_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() => runApp(const DigitalMenuApp());
 
--- mobile/lib/screens/more_screen.dart
+++ mobile/lib/screens/more_screen.dart
@@ -3,7 +3,9 @@ import 'QR/qr_screen.dart';
 import 'Setting/settings_screen.dart';
 
 class MoreScreen extends StatelessWidget {
-  const MoreScreen({super.key});
+  final Function(bool) onThemeChanged;
+
+  const MoreScreen({super.key, required this.onThemeChanged});
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +27,14 @@ class MoreScreen extends StatelessWidget {
             leading: const Icon(Icons.settings),
             title: const Text('Settings'),
             onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => const SettingsScreen()),
-              // );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsScreen(
+                    onThemeChanged: onThemeChanged, // ✅ pass the function
+                  ),
+                ),
+              );
             },
           ),
         ],
