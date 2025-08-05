deec0ff0746ff9d763e663dc93ab9977db1bc432
CHHOEURN.SREYNICH <CHHOEURN.SREYNICH@pnc.local>
2025-08-05 10:54:03 +0700

    Fix line endings (LF to CRLF) in generated Flutter platform registrant files

--- mobile/lib/services/api_services.dart
+++ mobile/lib/services/api_services.dart
@@ -10,7 +10,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
   static const String baseUrl =
-      'http://192.168.108.89:8000/api'; // Update with your preferred base URL
+      'http://192.168.108.93:8000/api'; // Update with your preferred base URL
 
   static String? _token;
 
