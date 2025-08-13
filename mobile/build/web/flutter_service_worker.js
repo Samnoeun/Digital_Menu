16f1e20c4ec070a480fdf23b35ed63670b93e46b
sreypich01 <sreypich.rom@student.passerellesnumeriques.org>
2025-08-13 10:44:44 +0700

    merge code in main

--- mobile/build/web/flutter_service_worker.js
+++ mobile/build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ccd2f1d622417c5c109db70a3ee85da4
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "910cb2ebd429a8bb15bc27f3b997c5e2",
+"flutter_bootstrap.js": "6306497639ff6ca3300e2c6043695ecc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
