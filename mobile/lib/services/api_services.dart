0f0523596a0a4ebe6b73b4b240721bde86a64fd5
chheunseangmeng <chheunseangmeng@gmail.com>
2025-08-17 14:32:03 +0700

    pull code

--- mobile/lib/services/api_services.dart
+++ mobile/lib/services/api_services.dart
@@ -11,7 +11,8 @@ import 'dart:typed_data'; // For Uint8List
 import 'package:flutter/foundation.dart'; // For kIsWeb
 
 class ApiService {
-  static const String baseUrl = 'https://qrmenu.zapto.org/api';
+  // static const String baseUrl = 'http://qrmenu.zapto.org/api';
+  static const String baseUrl = 'http://192.168.108.200:8000/api';
 
   static String? _token;
 
