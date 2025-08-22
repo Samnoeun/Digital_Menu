8b5c0209f4704f81cc8a115aec4b87520946949b
chheunseangmeng <chheunseangmeng@gmail.com>
2025-08-22 14:48:10 +0700

    added report page

--- backend/app/Http/Controllers/Api/OrderController.php
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\OrderHistory;
 use App\Models\OrderItemHistory;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -65,11 +65,3 @@ Route::middleware('auth:sanctum')->group(function () {
 
     
 });
-Route::get('/reports/sales-summary', [ReportController::class, 'salesSummary']);
-        // Order history routes
-    // routes/api.php
-// Route::get('/order-history', [OrderHistoryController::class, 'index']);
-
-
-
-    
