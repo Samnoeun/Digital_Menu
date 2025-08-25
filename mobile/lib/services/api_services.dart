bd28aac428d3a9b41e246f2ede089fa351d0eba1
neatsolin <neatsolin98@gmail.com>
2025-08-25 07:50:31 +0700

    fix pull

--- mobile/lib/services/api_services.dart
+++ mobile/lib/services/api_services.dart
@@ -14,7 +14,7 @@ import '../screens/ReportOrderHistory/report_order_screen.dart';
 
 
 class ApiService {
-  static const String baseUrl = 'http://10.24.192.127:8080/api';
+  static const String baseUrl = 'http://192.168.108.175:8000/api';
 
   static String? _token;
 
