762d1476e1f5d5052048c32f6cd5df7684001b8b
sreypich01 <sreypich.rom@student.passerellesnumeriques.org>
2025-08-29 15:38:06 +0700

    update improve feature and interface in web

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,19 +1,22 @@
+<?php
 
-    <?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\OrderController;
 
-    use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\Api\RestaurantController;
-    use App\Http\Controllers\Api\OrderController;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+// Public route for web menu preview
+Route::get('/restaurants/{id}/menu', [RestaurantController::class, 'webMenuPreview'])->name('web.menu');
 
-    // Public route for web menu preview
-    Route::get('/restaurants/{id}/menu', [RestaurantController::class, 'webMenuPreview'])->name('web.menu-preview');
+// Cart page route
+Route::get('/restaurants/{id}/cart', [OrderController::class, 'webCart'])->name('web.cart');
 
-    // Routes for table number input and order confirmation
-    Route::get('/restaurants/{id}/table-number', [OrderController::class, 'webTableNumber'])->name('web.table-number');
-    Route::post('/restaurants/{id}/submit-order', [OrderController::class, 'webSubmitOrder'])->name('web.submit-order');
-    Route::get('/restaurants/{id}/order-confirmation', [OrderController::class, 'webOrderConfirmation'])->name('web.order-confirmation');
-    Route::get('/images/{type}/{filename}', [App\Http\Controllers\Api\ImageController::class, 'show'])->name('image.show');
+// Routes for table number input and order confirmation
+Route::get('/restaurants/{id}/table-number', [OrderController::class, 'webTableNumber'])->name('web.table-number');
+Route::post('/restaurants/{id}/submit-order', [OrderController::class, 'webSubmitOrder'])->name('web.submit-order');
+Route::get('/restaurants/{id}/order-confirmation', [OrderController::class, 'webOrderConfirmation'])->name('web.order-confirmation');
+Route::get('/restaurants/{id}/check-table', [OrderController::class, 'webCheckTable'])->name('web.check-table');
+Route::get('/images/{type}/{filename}', [App\Http\Controllers\Api\ImageController::class, 'show'])->name('image.show');
