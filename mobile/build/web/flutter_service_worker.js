d272351e82d078965c17dccb2f6663c66384dfd3
chheunseangmeng <chheunseangmeng@gmail.com>
2025-09-06 14:20:38 +0700

    fixed the menu item page

--- mobile/build/web/flutter_service_worker.js
+++ mobile/build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "aa94d56b3e64572c2663c15556fa35a0
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "88778dd46ed9248f2fba049692ce30c9",
+"flutter_bootstrap.js": "c8758429806ce314f1302ffef2ca9ae4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
